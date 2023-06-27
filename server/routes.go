74ba9855498886e5ea21138a475f1b633ba1328c
Jan Fooken <git@bode.fun>
2023-06-27 08:44:52 +0200

    add protected example route

--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -10,6 +11,20 @@ import (
 func (server *baseServer) registerRoutes() {
 	server.router.GET("/", func(w http.ResponseWriter, req bunrouter.Request) error {
 		_, err := fmt.Fprint(w, "Hello, World!")
+		return err
+	})
+
+	protected := server.router.NewGroup("/protected").Use(func(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
+		return func(w http.ResponseWriter, req bunrouter.Request) error {
+			w.WriteHeader(http.StatusUnauthorized)
+			err := errors.New("auth middleware: not implemented")
+			fmt.Fprint(w, err.Error())
+			return err
+		}
+	})
+
+	protected.GET("", func(w http.ResponseWriter, req bunrouter.Request) error {
+		_, err := fmt.Fprint(w, "Hello, Protected!")
 		return err
 	})
 }
